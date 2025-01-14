12102a3943fd90e4001009013c9e2b95412b2079
Etienne Rivard <etienne@kerzo.ca>
2025-01-14 15:03:46 -0500

    vers le cloud

--- backup-mac.sh
+++ backup-mac.sh
@@ -1,20 +1,20 @@
 #!/bin/zsh
 
-rsync -ahP ~/Documents etienne@192.168.2.38:/home/etienne/mac-backup/
-rsync -ahP /Users/etiennerivard/Library/CloudStorage/Dropbox etienne@192.168.2.38:/home/etienne/mac-backup/
-rsync -ahP /Users/etiennerivard/Library/CloudStorage/OneDrive-CégepdeVictoriaville/CegepVicto etienne@192.168.2.38:/home/etienne/mac-backup/
-rsync -ahP --exclude='.git/' --exclude='node_modules/' --exclude='venv/' --exclude='sites/' --exclude='www/' ~/notes_de_cours etienne@192.168.2.38:/home/etienne/mac-backup/
-rsync -ahP --exclude='.git/' --exclude='node_modules/' --exclude='venv/' --exclude='sites/' --exclude='www/' ~/projets etienne@192.168.2.38:/home/etienne/mac-backup/
-rsync -ahP --exclude='.git/' --exclude='node_modules/' --exclude='venv/' --exclude='sites/' --exclude='www/' ~/demo_cours etienne@192.168.2.38:/home/etienne/mac-backup/
-rsync -ahP ~/.aliases etienne@192.168.2.38:/home/etienne/mac-backup/
-rsync -ahP ~/.mrconfig etienne@192.168.2.38:/home/etienne/mac-backup/
-rsync -ahP ~/.ssh etienne@192.168.2.38:/home/etienne/mac-backup/
+rsync -ahP ~/Documents root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
+rsync -ahP /Users/etiennerivard/Library/CloudStorage/Dropbox root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
+rsync -ahP /Users/etiennerivard/Library/CloudStorage/OneDrive-CégepdeVictoriaville/CegepVicto root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
+rsync -ahP --exclude='.git/' --exclude='node_modules/' --exclude='venv/' --exclude='sites/' --exclude='www/' ~/notes_de_cours root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
+rsync -ahP --exclude='.git/' --exclude='node_modules/' --exclude='venv/' --exclude='sites/' --exclude='www/' ~/projets root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
+rsync -ahP --exclude='.git/' --exclude='node_modules/' --exclude='venv/' --exclude='sites/' --exclude='www/' ~/demo_cours root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
+rsync -ahP ~/.aliases root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
+rsync -ahP ~/.mrconfig root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
+rsync -ahP ~/.ssh root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
 
 ~/scripts/generate-vscode-extensions-install.sh
-rsync -ahP --exclude='.git/' --exclude='node_modules/' --exclude='venv/' --exclude='sites/' --exclude='www/' ~/scripts etienne@192.168.2.38:/home/etienne/mac-backup/
+rsync -ahP --exclude='.git/' --exclude='node_modules/' --exclude='venv/' --exclude='sites/' --exclude='www/' ~/scripts root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
 
-rsync -ahP ~/Library/Application\ Support/Code/User/settings.json etienne@192.168.2.38:/home/etienne/mac-backup/vscode/
+rsync -ahP ~/Library/Application\ Support/Code/User/settings.json root@guizmo.profinfo.ca:/mnt/blockstorage/backup/vscode/
 
 brew deps --tree --installed > ~/homebrew-installed-packages.txt
-rsync -ahP ~/homebrew-installed-packages.txt etienne@192.168.2.38:/home/etienne/mac-backup/
+rsync -ahP ~/homebrew-installed-packages.txt root@guizmo.profinfo.ca:/mnt/blockstorage/backup/
 
